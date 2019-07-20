4528a19623df4a94f003c9f0d3839c552f063520
ping58972 <ping58972@gmail.com>
2019-07-20 00:09:22 -0500

    added mobile app project

--- src/js/compoments/MobileProject.js
+++ src/js/compoments/MobileProject.js
@@ -4,8 +4,12 @@ import SlideDisplay from './SlideDisplay';
 const MobileProject = () => {
   const projects = [
     {
-      title: 'Coming Soon',
-      text: 'Coming Soon'
+      title: 'Taylor Songs Mobile App',
+      subtitle: '| React | React-Native | Android Studio | ',
+      text:
+        'This mobile app is for practice react native that implement by android studio, etc.',
+      image: require('../../img/TaylorSongsApp.jpg'),
+      githubUrl: 'https://github.com/ping58972/Taylor-songs-mobile-app'
     }
   ];
 
